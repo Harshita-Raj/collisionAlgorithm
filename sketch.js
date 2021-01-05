1a4162ad40794a487ace28f4957fe4254b67a571
Harshita Raj <harshitarajj@gmail.com>
2021-01-05 17:07:25 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,8 +14,13 @@ function draw() {
   background(0,0,0);  
   movingRect.x = World.mouseX;
   movingRect.y = World.mouseY;
+  isTouching();
+ 
+  drawSprites();
+}
 
-  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
+function isTouching(){
+   if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
       && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2
       && movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
       && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2) {
@@ -26,5 +31,4 @@ function draw() {
     movingRect.shapeColor = "green";
     fixedRect.shapeColor = "green";
   }
-  drawSprites();
 }
